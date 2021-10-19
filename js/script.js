4d4e08e72d0b9f790410d6d2a711359826ea4639
Elli <elli.ludwin@gmail.com>
2021-10-19 16:16:12 -0400

    Edited script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,61 @@
-//create a function computerPlay() that will ranodmly return Rock, Paper, or Scissors
 const arr = ['Rock', 'Paper', 'Scissors'];
 
 function computerPlay() {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
-//take an input from the player in a string form of Rock, Paper, or Scissors
-const playerSelection = window.prompt("Enter Rock, Paper, or Scissors: ");
 
-console.log(playerSelection);
+function game() {
 
-const computerSelection = computerPlay();
-console.log(computerSelection);
-//create a function that takes to parameters playerSelection and computerSelection and returns a string that declares the winner of the round
-function resultRound(playerSelection, computerSelection){
-    //conditions where the player wins
-   if (playerSelection == "Rock" & computerSelection  == "Scissors" || 
-   playerSelection == "Paper" & computerSelection == "Rock" || 
-   playerSelection == "Scissors" & computerSelection == "Paper") {
-    return ("You win. Congrats!");
-    //conditions where the player loses
-   }else if (playerSelection == "Rock" &computerSelection == "Paper" || playerSelection == "Paper" & computerSelection == "Scissors" || playerSelection == "Scissors" & computerSelection == "Rock"){
-       return ("You lose. Loser!");
-   }else {
-       return ("Draw");
-   }
+let winCondition = 0;
+let loseCondition = 0;
+
+    for(let i=0; i<5; i++) {
+        const playerSelection = window.prompt("Enter Rock, Paper, or Scissors: ");
+        playerSelectionNew = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+        console.log(playerSelectionNew);
+
+        const computerSelection = computerPlay();
+        console.log(computerSelection);
+
+        function playRound(playerSelectionNew, computerSelection){
+    
+            if (playerSelectionNew == "Rock" && computerSelection  == "Scissors" || 
+            playerSelectionNew == "Paper" && computerSelection == "Rock" || 
+            playerSelectionNew == "Scissors" && computerSelection == "Paper") {
+                    return ("You win. Congrats!");
+                
+            }else if (playerSelectionNew == "Rock" &&computerSelection == "Paper" || playerSelectionNew == "Paper" && computerSelection == "Scissors" || playerSelectionNew == "Scissors" && computerSelection == "Rock"){
+                return ("You lose. Loser!");
+            }else if (playerSelectionNew == "Rock" &&computerSelection == "Rock" || playerSelectionNew == "Paper" && computerSelection == "Paper" || playerSelectionNew == "Scissors" && computerSelection == "Scissors") {
+                    return ("Draw");
+            }else {
+                return ("Invalid input. Enter Rock, Scissors, or Paper to play the game.");
+            }
+            }
+        console.log(playRound(playerSelectionNew, computerSelection));
+
+        const oneRound = playRound(playerSelectionNew, computerSelection);
+        if(oneRound == "You win. Congrats!"){
+            winCondition++;
+        }else if (oneRound == "You lose. Loser!") {
+            loseCondition++;
+        }
+        console.log(winCondition);
+        console.log(loseCondition);
+
+        if(winCondition >= 3){
+            console.log("YOU WON");
+            break;
+        }else if(loseCondition >= 3){
+            console.log("ALL IS LOST");
+            break;
+        }else if(loseCondition == winCondition){
+            console.log("DRAW");
+            continue;
+        }
+        
+    }
 }
-   
-   
-console.log(resultRound(playerSelection,computerSelection))
+
+game();
